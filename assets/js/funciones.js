f3931af91b10b01c260389a132e33bbb534e50fe
jeremy-andres <jaherreraquilodran@gmail.com>
2023-04-20 13:32:45 -0400

    priemr commit

---
+++ assets/js/funciones.js
@@ -0,0 +1,12 @@
+//fecha de naciemito del usuario
+let year = document.getElementById("year");
+let month = document.getElementById("month");
+let day = document.getElementById("day");
+
+let resultado = document.getElementById("resultado");
+
+function diaNacimiento() {
+    resultado.innerHTML = day 
+}
+
+diaNacimiento();
